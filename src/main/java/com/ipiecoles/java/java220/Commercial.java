0da904995f9b95779920a0691c47d2df55a4d6ef
Mathis OLLIER <maol.devpro@gmail.com>
2020-10-08 14:11:23 +0200

    exo 205 fait

--- src/main/java/com/ipiecoles/java/java220/Commercial.java
+++ src/main/java/com/ipiecoles/java/java220/Commercial.java
@@ -2,6 +2,8 @@ package com.ipiecoles.java.java220;
 
 import org.joda.time.LocalDate;
 
+import java.util.Objects;
+
 public class Commercial  extends Employe {
     private Double caAnnuel = 0.0;
 
@@ -11,6 +13,10 @@ public class Commercial  extends Employe {
 
     public void setCaAnnuel(Double caAnnuel) {
         this.caAnnuel = caAnnuel;
+    }
+
+    public Commercial(){
+        super();
     }
 
     @Override
@@ -24,5 +30,14 @@ public class Commercial  extends Employe {
     public Commercial(String nom, String prenom, String matricule, LocalDate dateEmbauche, Double salaire, Double caAnnuel) {
         super(nom, prenom, matricule, dateEmbauche, salaire);
         this.caAnnuel = caAnnuel;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        if (!super.equals(o)) return false;
+        Commercial that = (Commercial) o;
+        return Objects.equals(caAnnuel, that.caAnnuel);
     }
 }
