5013d314a463cafcb103fa3fe5e5660823c19fb8
Mathis OLLIER <maol.devpro@gmail.com>
2020-10-08 13:34:36 +0200

    exo 202 fait

--- src/main/java/com/ipiecoles/java/java220/Commercial.java
+++ src/main/java/com/ipiecoles/java/java220/Commercial.java
@@ -1,4 +1,13 @@
 package com.ipiecoles.java.java220;
 
-public class Commercial extends Employe {
+public class Commercial  extends Employe {
+    private Double caAnnuel = 0.0;
+
+    public Double getCaAnnuel() {
+        return caAnnuel;
+    }
+
+    public void setCaAnnuel(Double caAnnuel) {
+        this.caAnnuel = caAnnuel;
+    }
 }
