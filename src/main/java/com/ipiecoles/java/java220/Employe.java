8d81035d340e2cc72741ca95d80e25fe321dcdb1
Mathis OLLIER <maol.devpro@gmail.com>
2020-10-08 11:35:09 +0200

    exo 109 fait

--- src/main/java/com/ipiecoles/java/java220/Employe.java
+++ src/main/java/com/ipiecoles/java/java220/Employe.java
@@ -77,4 +77,12 @@ public class Employe {
     public int hashCode() {
         return Objects.hash(nom, prenom, matricule, dateEmbauche, salaire);
     }
+
+    public void augmenterSalaire(Double pourcentage){
+        this.salaire = this.getSalaire() * (1 + pourcentage);
+    }
+
+  //  public getPrimeAnnuelle(){
+  //      return Entreprise.primeAnnuelleBase();
+  //  }
 }
